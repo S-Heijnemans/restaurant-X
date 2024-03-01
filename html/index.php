9fcdaf780293909d8f0850ab7f302001c8904c32
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2024-03-01 12:13:32 +0100

    dingen op git krijgen
    alles wat ik al gemaakt had op github setten

---
+++ html/index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <div class='opmaakblok-header-index'></div>
+        <div class='logo-header'>
+            <a class='logo'>IJsPaleis</a>
+        </div>
+        <div class='opmaakblok-header-index-2'></div>
+    </header>
+    <nav>
+        <div class='nav-pages-balk'>
+            <a class='text-nav-index'>Home</a>
+            <a class='text-nav-index'>Nieuws</a>
+            <a class='text-nav-index'>Menu</a>
+            <a class='text-nav-index'>Contacten</a>
+        </div>
+        <div class="login--button-nav">
+            <a class='login-text'>Login</a>
+        </div>
+    </nav>
+    <div class='main-page'>
+        <div class="img-line-1">
+            <img src="assets/image-landing-1.jpg" height="100px" width="100px" alt="image nt loading">
+        </div>
+        <div class="img-line-2"></div>
+
+    </div>
+    
+</body>
+</html>
